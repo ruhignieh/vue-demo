23327741a7fdfe439abd4808ae413f4aa5f5033e
Ruhig Nieh <ruhignieh@RuhigdeMacBook-Pro.local>
2018-10-30 01:44:15 +0800

    create slot demo

---
+++ src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="list">
+    <div class="title">{{ title }}</div>
+    <div class="list">
+      <div class="list-item" v-for="item in items" :key="item.name">
+        <slot name="shape" v-bind="item"></slot>
+        <slot name="color" v-bind="item"></slot>
+        <slot v-bind="item"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'items']
+}
+</script>
+
---
+++ src/views/Component.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="component">
+    <h1>This is an component page</h1>
+    <list :items="shapes" title="Shapes">
+      <template scope="shape">
+        <div>{{shape.name}}</div>
+        <small>{{shape.sides}}sides</small>
+      </template>
+    </list>
+    <br>
+    <list :items="colors" title="Colors">
+      <template scope="color">
+        <div>{{color.name}}</div>
+        <small>hex:{{color.hex}}</small>
+      </template>
+    </list>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import List from "@/components/List.vue";
+
+export default {
+  name: "home",
+  components: {
+    List
+  },
+  data() {
+    return {
+      shapes: [
+        { name: 'Square', sides: 4 }, 
+        { name: 'Hexagon', sides: 6 }, 
+        { name: 'Triangle', sides: 3 }
+      ],
+      colors: [
+        { name: 'Yellow', hex: '#F4D03F', },
+        { name: 'Green', hex: '#229954' },
+        { name: 'Purple', hex: '#9B59B6' }
+      ]
+    }
+  }
+};
+</script>
